cb628011ec735b9bacccc2c0b809406e461633e0
Nick Chadwick <chadnickbok@gmail.com>
2023-12-04 13:09:54 -0800

    Day1 pt2

--- day1.py
+++ day1.py
@@ -5,9 +5,34 @@ import re
 with open("day1.txt") as f:
     lines = f.readlines()
 
+vals = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9
+}
+
 def get_calibration(line):
-    matches = re.findall("\d", line)
-    print(matches)
+    matches = []
+    for i in range(0, len(line)):
+        m = re.match("\d|one|two|three|four|five|six|seven|eight|nine", line[i:])
+        if m:
+            matches.append(vals[m[0]])
+    print(line, matches)
     return 10 * int(matches[0]) + int(matches[-1])
 
 calibrations = []
