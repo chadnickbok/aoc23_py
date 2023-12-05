7256c5f27bd630e8d0141e83ff238817746f1254
Nick Chadwick <chadnickbok@gmail.com>
2023-12-05 13:05:28 -0800

    day4 pt2

--- day4.py
+++ day4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 
 def parse_card(line):
     card_parts = line.split(":")[1].split("|")
@@ -10,20 +11,28 @@ def parse_card(line):
     score = 0
     for n in numbers_you_have:
         if n in winning_numbers:
-            if score == 0:
-                score = 1
-            else:
-                score = score * 2
+            score += 1
     return score
 
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-scores = []
-for line in lines:
-    score = parse_card(line.strip())
-    print(score)
-    scores.append(score)
 
-print(sum(scores))
+counts = {0: 1}
+for i in range(0, len(lines)):
+    if i not in counts:
+        counts[i] = 1
+    cur_count = counts[i]
+
+    c = parse_card(lines[i].strip())
+
+    for j in range(0, c):
+        cur_card = i + j + 1
+        if cur_card in counts:
+            counts[cur_card] += cur_count
+        else:
+            counts[cur_card] = cur_count + 1
+
+print(json.dumps(counts, indent=2))
+print(sum(counts.values()))
