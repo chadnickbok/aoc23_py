eb69291b8b545597b10cbf7295afd32d6202d458
Nick Chadwick <chadnickbok@gmail.com>
2023-12-08 11:49:32 -0800

    day8 pt2

--- day8.py
+++ day8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 
 class Node():
     def __init__(self, name, left, right):
@@ -8,6 +9,8 @@ class Node():
         self.left = left
         self.right = right
 
+    def __repr__(self):
+        return f"{self.name} = ({self.left}, {self.right})"
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
@@ -25,19 +28,40 @@ for line in lines[2:]:
     nodes[name] = Node(name, left, right)
 
 
-start = nodes["AAA"]
-print(start.name, start.left, start.right)
+#start = nodes["AAA"]
+#print(start.name, start.left, start.right)
 
-cur = start
-steps = 0
-while cur.name != "ZZZ":
-    c = commands[steps % len(commands)]
-    if c == "L":
-        cur = nodes[cur.left]
-    else:
-        cur = nodes[cur.right]
+#cur = start
+#steps = 0
+#while cur.name != "ZZZ":
+#    c = commands[steps % len(commands)]
+#    if c == "L":
+#        cur = nodes[cur.left]
+#    else:
+#        cur = nodes[cur.right]
+#
+#    steps += 1
 
-    steps += 1
+#print(steps)
 
-print(steps)
+starts = []
+for node_name in nodes:
+    if node_name.endswith("A"):
+        starts.append(nodes[node_name])
 
+ends = []
+for cur in starts:
+    steps = 0
+    
+    while not cur.name.endswith("Z"):
+        c = commands[steps % len(commands)]
+        if c == "L":
+            cur = nodes[cur.left]
+        else:
+            cur = nodes[cur.right]
+        steps += 1
+
+    print(steps)
+    ends.append(steps)
+
+print(math.lcm(*ends))
