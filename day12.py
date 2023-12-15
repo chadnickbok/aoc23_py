dd4a1fd653b3aa53ed2db86fff78c5c0125cd4cd
Nick Chadwick <chadnickbok@gmail.com>
2023-12-14 21:17:30 -0800

    day13 pt1

--- day12.py
+++ day12.py
@@ -1,133 +1,68 @@
 #!/usr/bin/env python3
 
 import sys
-import re
+import json
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-def is_valid(s, runs):
-    counts = []
-    cur = s[0]
-    count = 0
-    for c in s[1:]:
-        if cur == "#":
-            count += 1
-        elif count > 0:
-            counts.append(count)
-            count = 0
-        cur = c
+inputs = [line.strip().split() for line in lines]
 
-    if cur == "#":
-        count += 1
-    if count > 0:
-        counts.append(count)
+def get_possibilities(s, sizes, count, cache):
+    key = json.dumps((s, sizes, count))
+    if key in cache:
+        return cache[key]
 
-    if len(counts) != len(runs):
-        #print("len counts:", len(counts), "len runs:", len(runs))
-        return False
-
-    for i, count in enumerate(counts):
-        if count != runs[i]:
-            #print(counts, runs)
-            return False
-    return True
-
-
-def is_already_invalid(s, runs):
-    total_hashes = sum(runs)
-    cur_potential_hashes = s.count("#") + s.count("?")
-
-    if cur_potential_hashes < total_hashes:
-        return True
-
-    pattern = "[\.\?]*"
-    for run in runs:
-        pattern += f"[#\?]{{{run}}}[\.\?]*"
-    r = re.compile(pattern)
-
-    if r.search(s) is None:
-        #print(s, pattern)
-        return True
-
-    cur_s = s[:s.find("?")]
-
-    counts = []
-    cur = s[0]
-    count = 0
-
-    for c in cur_s[1:]:
-        if cur == "#":
-            count += 1
-        elif count > 0:
-            counts.append(count)
-            count = 0
-        cur = c
-
-    if cur == "#":
-        count += 1
-    if count > 0:
-        counts.append(count)
-
-    if len(counts) == 0:
-        return False
-
-    if len(counts) > len(runs):
-        return True
-
-    for i in range(0, len(counts) - 1):
-        if counts[i] != runs[i]:
-            return True
-
-    if counts[-1] > runs[len(counts) - 1]:
-        return True
-
-    return False
-
-
-def get_positions(s, runs):
-
-    i = s.find('?')
-    if i >= 0:
-        if is_already_invalid(s, runs):
+    if len(s) == 0:
+        # Reached the end of the string
+        if len(sizes) == 0 and count == 0:
+            # This is a possibility
+            cache[key] = 1
+            return 1
+        else:
+            # Dead end
+            cache[key] = 0
             return 0
-        return get_positions(s[:i] + "." + s[i+1:], runs) + get_positions(s[:i] + "#" + s[i+1:], runs)
 
-    if is_valid(s, runs):
-        #print(s, "Valid")
-        return 1
+    if s[0] == "?":
+        possible_chars = ["#", "."]
     else:
-        #print(s, "Invalid")
-        return 0
+        possible_chars = [s[0]]
+
+    n = 0
+    for c in possible_chars:
+        if c == "#":
+            # Increase the current count and keep going
+            n += get_possibilities(s[1:], sizes, count + 1, cache)
+            continue
+
+        if count != 0 and len(sizes) > 0 and count == sizes[0]:
+            # Current group is good, keep going
+            n += get_possibilities(s[1:], sizes[1:], 0, cache)
+        elif count == 0:
+            n += get_possibilities(s[1:], sizes, 0, cache)
+
+    cache[key] = n
+    return n
 
 
 total = 0
-for line in lines:
-    parts = line.strip().split()
-    springs = parts[0]
-    runs = [int(x) for x in parts[1].split(",")]
+for t in inputs:
+    s = t[0] + "."
+    sizes = [int(x) for x in t[1].split(",")]
 
-    valid_positions = get_positions(springs, runs)
-    total += valid_positions
+    p = get_possibilities(s, sizes, 0, {})
+    print(s, sizes, p)
+    total += p
 
 print(total)
 
 total = 0
-for line in lines:
-    parts = line.strip().split()
-    springs = parts[0]
-    runs = [int(x) for x in parts[1].split(",")]
+for t in inputs:
+    s = "?".join([t[0]] * 5) + "."
+    sizes = [int(x) for x in t[1].split(",")] * 5
 
-    print(springs)
-    
-    expanded_springs = []
-    expanded_runs = []
-    for i in range(0, 5):
-        expanded_runs += runs
-        expanded_springs.append(springs)
-    expanded_springs = "?".join(expanded_springs)
-
-    valid_positions = get_positions(expanded_springs, expanded_runs)
-    total += valid_positions
-
+    p = get_possibilities(s, sizes, 0, {})
+    print(s, sizes, p)
+    total += p
 print(total)
--- day13.py
+++ day13.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
@@ -25,25 +26,18 @@ def test_reflection(s, i):
     l = s[:i][::-1]
     r = s[i:]
 
+    print(l, r)
     for i in range(0, min(len(l), len(r))):
         if l[i] != r[i]:
             return False
     return True
 
 
-def reflects_horizontally(l, r):
-    if len(l) != len(r):
-        print("Sliced wrong!")
-
-    for i, c in enumerate(r):
-        if a[-i - 1] != c:
-            return False
-    return True
-
+totals = 0
 
 for block in blocks:
     possibles = []
-    for i in range(1, len(block[0]) - 1):
+    for i in range(1, len(block[0]) ):
         possibles.append(i)
     for _, l in enumerate(block):
         cur_possibles = []
@@ -51,6 +45,39 @@ for block in blocks:
             if test_reflection(l, i):
                 cur_possibles.append(i)
         possibles = cur_possibles
+        print(possibles)
     
     if len(possibles) > 0:
         print(possibles)
+        totals += possibles[0]
+        continue
+
+    rotated = []
+    for i in range(0, len(block[0])):
+        rotated.append([])
+
+    for j in range(0, len(block)):
+        for i in range(0, len(block[0])):
+            rotated[i].append(block[j][i])
+
+    possibles = []
+    for i in range(1, len(rotated[0])):
+        possibles.append(i)
+    for _, l in enumerate(rotated):
+        cur_possibles = []
+        for i in possibles:
+            if test_reflection(l, i):
+                cur_possibles.append(i)
+        possibles = cur_possibles
+        print(possibles)
+
+    if len(possibles) > 0:
+        totals += possibles[0] * 100
+    else:
+        print("Broken block")
+        for l in rotated:
+            print(l)
+
+
+print(totals)
+
